859a5f243206e55e73a3e84eca35d0cac0f8b732
Matthew Ramir <matthew_ramir@freddiemac.com>
2021-08-31 20:27:03 +0000

    Theia keymap updates.

--- supportFiles/acli-setup.sh
+++ supportFiles/acli-setup.sh
@@ -2,7 +2,7 @@
 ## Setuo script for acquia cloud ide
 
 rm -f ~/.theia/keymap.json
-ln -s ~/zsh/consoleConfig/theia-keymap.json ~/.theia/keymap.json
+ln -s ~/zsh/consoleConfig/theia-keymaps.json ~/.theia/keymaps.json
 
 rm -f ~/.bash_aliases
 ln -s ~/zsh/.bash_aliases ~/.bash_aliases
